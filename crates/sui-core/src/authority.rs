abf4297e9d399caeea2764fc5555542e41f84e9f
sadhan <sadhan@mystenlabs.com>
2023-03-30 00:43:32 -0700

    Allow configurable pruning delay and add some metrics

--- crates/sui-core/src/authority.rs
+++ crates/sui-core/src/authority.rs
@@ -1635,6 +1635,7 @@ impl AuthorityState {
             store.objects_lock_table.clone(),
             pruning_config,
             epoch_store.epoch_start_state().epoch_duration_ms(),
+            prometheus_registry,
         );
         let state = Arc::new(AuthorityState {
             name,
